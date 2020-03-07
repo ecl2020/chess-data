965eccc1ac4dff1cdb4a84697f45944de678e533
ecl2020 <34615588+ecl2020@users.noreply.github.com>
2020-03-06 23:05:31 -0500

    link tables

--- plots.js
+++ plots.js
@@ -1,92 +1,120 @@
-// set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 1000 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+function bounds(max) {
+    return [max * 0.05, max - (max * 0.05)]
+}
 
-var svg = d3.select("#cmr_plot")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
 
-d3.tsv("https://raw.githubusercontent.com/ecl2020/chess-data/master/data/cmr.txt", function (error, data) {
-    if (error) throw error;
-    data.forEach(function (d) {
-        name = +d.Name;
-        rating = +d.Rating;
-        year = +d.Year;
-    })
-    console.log(data[0])
-    console.log(data[30])
-    var x = d3.scaleLinear()
-        .domain([d3.min(data, d => d.Year), d3.max(data, d => d.Year)])
-        .range([10, width - 10]);
+function plot(address, plot, plot_title) {
+    var margins = { top: $(document).height() * 0.05, bottom: $(document).height() * 0.05 }
+    var height = $(document).height() - margins.top;
+    var width = $(document).width();
 
-    var y = d3.scaleLinear()
-        .domain([d3.min(data, d => d.Rating), d3.max(data, d => d.Rating)])
-        .range([height - 10, 10])
+    // var svg = d3.select(#cmr_plot)
+    var svg = d3.select(plot).append("svg")
+        .attr("width", width)
+        .attr("height", height)
 
-    var x_axis = d3.axisBottom()
-        .tickFormat(d3.format("d"))
-        .scale(x);
-
-    var y_axis = d3.axisLeft()
-        .scale(y)
-
-    svg.append("g")
-        .attr("transform", "translate(50, 10)")
-        .call(y_axis)
-
-    var xAxisTranslate = height - 10;
-
-    svg.append("g")
-        .attr("transform", "translate(50, " + xAxisTranslate + ")")
-        .call(x_axis)
-
-    var div = d3.select("#cmr_plot").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0)
-
-    svg.selectAll('circle').data(data)
-        .enter().append('circle')
-        .attr('cx', function (d) { return x(d.Year) + 50 })
-        .attr('cy', function (d) { return y(d.Rating) })
-        .attr('r', 1.5)
-        .style('fill', "#33b5e5")
-        .on('mouseover', function (d, i) {
-            d3.select(this)
-                .attr('r', 3)
-                .style('fill', '33e5b5')
-            div.transition()
-                .duration('100')
-                .style("opacity", "1")
-            div.html(d.Name + "was rated " + d.Rating + " in " + d.Year)
-                .style("left", (event.clientX + 15) + "px")
-                .style("top", (event.clientY + 15)  + "px")
-                .style("position", "absolute")
-                .style("background-color", "gray")
-                .style("color", "white")
-                .style("padding", "3px")
-                .style("border-radius", "5px")
-                
+    // d3.tsv("https://raw.githubusercontent.com/ecl2020/chess-data/master/data/cmr.txt", function(error, data))
+    d3.tsv(address, function (error, data) {
+        if (error) throw error;
+        data.forEach(function (d) {
+            name = +d.Name;
+            rating = +d.Rating;
+            year = +d.Year;
         })
-        .on('mouseout', function (d, i) {
-            d3.select(this)
-                .attr('r', 1.5)
-                .style('fill', "#33b5e5")
-            div.transition()
-                .duration('100')
-                .style("opacity", "0")
-        });
+        console.log(data[40])
+        var x = d3.scaleLinear()
+            .domain([d3.min(data, d => d.Year), d3.max(data, d => d.Year)])
+            // .range(bounds(width))
+            .range([10, width - 10]);
 
-    svg.append("text")
-        .attr("x", (width / 2) + 50)
-        .attr("y", 10)
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .style("text-decoration", "underline")
-        .text("ChessMetrics Ratings")
+        var y = d3.scaleLinear()
+            .domain([d3.min(data, d => d.Rating), d3.max(data, d => d.Rating)])
+            // .range(bounds(height)[1], bounds(height)[0])
+            .range([height - 10, 10])
 
-})
+        var x_axis = d3.axisBottom()
+            .tickFormat(d3.format("d"))
+            .scale(x);
 
+        var y_axis = d3.axisLeft()
+            .scale(y)
 
+        var xAxisTranslate = height - 30;
+
+        svg.append("g")
+            .attr("transform", "translate(50, 10)")
+            // .attr("transform", "translate(" + bounds(height)[1] + "," + bounds(height)[0] + ")")
+            .call(y_axis)
+
+        svg.append("g")
+            // .attr("transform", "translate(" + bounds(width) + ")")
+            .attr("transform", "translate(40, " + xAxisTranslate + ")").call(x_axis)
+
+        var div = d3.select(plot).append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0)
+
+        svg.selectAll('circle').data(data)
+            .enter().append('circle')
+            .attr('cx', function (d) { return x(d.Year) + 50 })
+            .attr('cy', function (d) { return y(d.Rating) })
+            .attr('r', 1.5)
+            .style('fill', "#33b5e5")
+            .on('mouseover', function (d, i) {
+                d3.select(this)
+                    .attr('r', 4)
+                    .style('fill', '33e5b5')
+                div.transition()
+                    .duration('100')
+                    .style("opacity", "1")
+                    .style(cursor = "none")
+                div.html(d.Name + " was rated " + d.Rating + " in " + d.Year)
+                    .style("left", (event.clientX) + "px")
+                    .style("top", (event.clientY) + "px")
+                    .style("position", "absolute")
+                    .style("background-color", "gray")
+                    .style("color", "white")
+                    .style("padding", "3px")
+                    .style("border-radius", "5px")
+
+            })
+            .on('mouseout', function (d, i) {
+                d3.select(this)
+                    .attr('r', 1.5)
+                    .style('fill', "#33b5e5")
+                div.transition()
+                    .duration('100')
+                    .style("opacity", "0")
+            });
+
+        // svg.append("text")
+        //     .attr("x", (width / 2) + 50)
+        //     .attr("y", 10)
+        //     .attr("text-anchor", "middle")
+        //     .style("font-size", "16px")
+        //     .style("text-decoration", "underline")
+        //     .text(plot_title)
+
+    })
+}
+
+function pages(current) {
+    var x = document.getElementById("myTable").rows[0].cells;
+    var i = 0
+    if (current != "home") {
+        x[i].innerHTML = '<a href="' + "./index.html" + '">' + "Home" + '</a>';
+        i++
+    }
+    if (current != "hist") {
+        x[i].innerHTML = '<a href="' + "./fide_hist.html" + '">' + "Historic FIDE Ratings" + '</a>';
+        i++
+    }
+    if (current != "edo") {
+        x[i].innerHTML = '<a href="' + "./edopage.html" + '">' + "Edo Page" + '</a>';
+        i++
+    }
+    if (current != "cmr") {
+        x[i].innerHTML = '<a href="' + "./cmrpage.html" + '">' + "CMR" + '</a>';
+    }
+}
 
